4f8e93831811e52def085fb40167e1f7beac55c0
Chandra Bhushan Sah <123019803+Chandus2003@users.noreply.github.com>
2025-08-07 22:55:33 +0530

    adding minor change

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -35,7 +35,7 @@ export default function Login() {
             type="submit"
             className="w-full bg-blue-600 hover:bg-blue-700 py-2 rounded font-semibold"
           >
-            Login to th app
+            Login
           </button>
         </form>
       </div>
